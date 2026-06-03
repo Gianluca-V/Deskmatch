d6e91bf8287d5971ea816b821d60d5d527e58d9d
sergiorock <lopez.sergio.n@gmail.com>
2026-06-03 18:41:57 -0300

    revert: se revierte frontend-admin a estado original de la rama

--- apps/frontend-admin/src/api/offices.js
+++
@@ -1,7 +0,0 @@
-import client from './client';
-
-export const createOffice = (data) => client.post('/api/offices', data).then((r) => r.data);
-
-export const getOffices = (params) => client.get('/api/offices', { params }).then((r) => r.data);
-
-export const getOffice = (id) => client.get(`/api/offices/${id}`).then((r) => r.data);
